795288cf589d3f46e3f0edab14682cee129bc440
steelone <slavikdnsnk@gmail.com>
2021-03-16 23:33:17 +0300

    fixed selectors

--- src/app/reducers/count/count.selectors.ts
+++ src/app/reducers/count/count.selectors.ts
@@ -3,10 +3,10 @@ import { countNode, CountState } from "./count.reducer";
 
 export const selectCountFeature =  createFeatureSelector<CountState>(countNode)
 
-export const selectCount = createSelector(selectCountFeature, ()=>{
+export const selectCount = createSelector(selectCountFeature, 
     (state: CountState): number => state.count
-})
+)
 
-export const selectUpdatedAt = createSelector(selectCountFeature, ()=>{
+export const selectUpdatedAt = createSelector(selectCountFeature, 
     (state: CountState): number => state.updatedAt
-})
+)
